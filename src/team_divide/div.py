165e3ce3ce51bee9e0491b854eaebe6fbdb4fb7b
Kimwonjoon <kimpass189@gmail.com>
2024-07-17 10:21:16 +0900

    add

--- src/team_divide/div.py
+++ src/team_divide/div.py
@@ -1,2 +1,9 @@
-def div():
-    print(50/10)
+import sys
+def div(a,b):
+    print(a/b)
+def main():
+    a = int(sys.argv[1])
+    b = int(sys.argv[2])
+    div(a,b)
+if __name__ == "__main__":
+    main()
